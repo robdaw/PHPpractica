e52a0ea0442acbf7c981c0368c28ec4b32ba2af9
Francisco José Gómez Arnal <fjgomez@florida-uni.es>
2016-09-21 23:05:09 +0200

    Actividad 03

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+      <title>ACTIVIDAD 03</title>
+  </head>
+  <body>
+    <?php
+      echo "Aquí poner vuestro nombre";
+      echo "Aquí poner vuestro primer apellido";
+      echo "Aquí poner vuestro segundo apellido";
+    ?>
+  </body>
+</html>
