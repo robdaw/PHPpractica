7096eed315bb8681b2eba3350f989d9f73ca441d
Roberto jesus Sanchez Camacho <rosaca01@floridauniversitaria.es>
2016-09-22 09:37:32 +0200

    primera fase

--- index.php
+++ index.php
@@ -1,13 +1,17 @@
+<?php
 //fichero index.php
 //creado por Paco Gomez
+?>
 <html>
   <head>
       <title>ACTIVIDAD 03</title>
-      <?php
-        echo "Aquí poner vuestro nombre;
-        echo "Aquí poner vuestro primer apellido";
-        print "Aquí poner vuestro segundo apellido";
-      ?>
+    
   </head>
   <body>
+    <?php
+      echo "Roberto<br>";
+      echo "Sánchez<br>";
+      print "Camacho<br>";
+    ?>
+  </body>
 </html>
